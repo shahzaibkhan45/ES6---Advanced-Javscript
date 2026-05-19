d6dec53f4d04b668b23d6e99c03d3064aa18153a
SHAHZAIB <shahzaibkhanmsk1@gmail.com>
2026-05-19 13:54:59 +0500

    update local and global scope

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //local and global scope
 if(true){
-    let a = 10;// let is also a local scope only run in function and block
+    let a = 10;// let is also a local scope only run in function and block loop ke andar chalega but bahir nahi chalega for loop me bhi chalta hai
 }
 console.log(a);
 if(true){
-    const a = 10;// const is also local scope only run in function and block
+    const a = 10;// const is also local scope only run in function and block loop ke andar chalega but bahir nahi chalega for loop me nahi chalega
 }
 console.log(a);
 if(true){
-    var a = 10;// var is global scope run in function and also in outside of function
+    var a = 10;// var is global scope run in function and also in outside of function loop ke bahir/andar bhi chaega 
 }
 console.log(a);
