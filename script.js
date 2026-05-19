cbe2b36e2fa51ca4ad3edc5bd852863cfb9f0613
SHAHZAIB <shahzaibkhanmsk1@gmail.com>
2026-05-19 13:30:55 +0500

    branch one

---
+++ script.js
@@ -0,0 +1,14 @@
+// let var const 
+//var 
+var a = 10;
+var a = 20;// in var we can redeclare and reassign the value
+console.log(a);
+
+//let
+let b = 30;
+b = 40;// in let we can reassign the value but we cannot redeclare the variable
+console.log(b);
+
+//const
+const c = 60;// in const we cannot redeclare and reassign the value
+console.log(c);
